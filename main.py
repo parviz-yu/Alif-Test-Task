9664453f96154eac3abac1d830a5cea78442e2ef
Parviz Yuldashev <www.parviz98@ya.ru>
2022-09-19 22:51:06 +0500

    PEP8 formating and adding description for functions

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 import os
 
 
-
-
 @dataclass
 class Product:
     name: str
@@ -15,16 +13,17 @@ class ProductList:
     def __init__(self) -> None:
         self.products = []
 
-
     def add_item(self, file_name: str) -> None:
+        """Данная функция добавляет продукты в список"""
+
         if self.__is_file_exist(file_name):
             with open(file_name, 'a', encoding='utf-8') as curr_file:
-                print('Для завершения ввода нажмите "q"')
+                print('\nРежим добавления продуктов. Для завершения ввода нажмите "q"')
                 curr_file.write('\n')
                 for line in sys.stdin:
                     if line.rstrip() == 'q':
                         break
-                    *_, price = line.split(' ')
+                    *_, price = line.split(' - ')
                     if float(price) > 0:
                         print(line, end='', file=curr_file)
                     else:
@@ -33,27 +32,31 @@ class ProductList:
         else:
             print('Файл не найден.')
 
-
     def calc_sum(self, file_name: str) -> None:
+        """Данная функция считает общую стоимость продуктов"""
+
         total = 0
         self.__read_file(file_name)
         for item in self.products:
             total += item.price
-        print(total)
-
+        print(f'Общая сумма: {total}')
 
     def edit_item(self, file_name):
+        """Данная функция изменяет продукты в списоке"""
+
         if self.__is_file_exist(file_name):
             print('\nРежим изменения списка.')
             is_completed = False
             self.__read_file(file_name)
-            n, _, p = input('Параметры товара, которые хотите изменить в формате (Имя - цена): ').split(' ')
-            n_change, _, p_change = input('Новые параметры товара в формате (Имя - цена): ').split(' ')
+            n, p = input(
+                'Параметры товара, которые хотите изменить в формате (Имя - цена): ').split(' - ')
+            n_change, p_change = input(
+                'Новые параметры товара в формате (Имя - цена): ').split(' - ')
             for i in range(len(self.products)):
                 if self.products[i].name == n and self.products[i].price == float(p):
                     self.products[i].name, self.products[i].price = n_change, p_change
                     is_completed = True
-            
+
             if is_completed:
                 self.__rewrite_file(file_name)
                 print('Товар успешно изменен')
@@ -62,18 +65,20 @@ class ProductList:
         else:
             print('Файл не найден.')
 
-
     def delete_item(self, file_name: str) -> None:
+        """Данная функция удаляет продукты из списка"""
+
         if self.__is_file_exist(file_name):
             print('\nРежим удаления товара.')
             is_completed = False
             self.__read_file(file_name)
-            n, _, p = input('Параметры товара в формате (Имя - цена): ').split(' ')
+            n, p = input(
+                'Параметры товара в формате (Имя - цена): ').split(' - ')
             for i in range(len(self.products)):
                 if self.products[i].name == n and self.products[i].price == float(p):
                     self.products[i] = ''
                     is_completed = True
-            
+
             if is_completed:
                 self.__rewrite_file(file_name)
                 print('Товар успешно удален.')
@@ -82,26 +87,29 @@ class ProductList:
         else:
             print('Файл не найден.')
 
-
     def __rewrite_file(self, file_name: str) -> None:
+        """Приватная функция для записи изменений в файл"""
+
         with open(file_name, 'w', encoding='utf-8') as output:
             for prod in self.products:
                 if prod:
-                    print(f'{prod.name} — {prod.price}', end='\n', file=output)
-
+                    print(f'{prod.name} - {prod.price}', end='\n', file=output)
 
     def __read_file(self, file_name: str) -> None:
+        """Приватная функция считывает файл и сохраняет продукты как объект Product"""
+
         if self.__is_file_exist(file_name):
             with open(file_name, 'r', encoding='utf-8') as file:
                 for line in file:
                     if line.strip():
-                        name, _, price = line.split(' ')
+                        name, price = line.split(' - ')
                         self.products.append(Product(name, float(price)))
         else:
             print('Файл не найден')
 
-
     def __is_file_exist(self, filename: str) -> bool:
+        """Приватная функция проверяет существование файла в директории"""
+
         split = filename.split('/')
         if len(split) != 1:
             os.chdir('/'.join(split[:-1]))
@@ -115,18 +123,18 @@ def main() -> None:
 
     product_list = ProductList()
     COMMANDS = {
-    '--Добавить-в-список': product_list.add_item,
-    '--Изменить-запись-в-списке': product_list.edit_item,
-    '--Удалить-из-списка': product_list.delete_item,
-    '--Вычесть-общую-сумму': product_list.calc_sum
+        '--Добавить-в-список': product_list.add_item,
+        '--Изменить-запись-в-списке': product_list.edit_item,
+        '--Удалить-из-списка': product_list.delete_item,
+        '--Вычеслить-общую-сумму': product_list.calc_sum
     }
 
     if command not in COMMANDS:
         print('\nНеправильный ввод. Используйте правильные команды.\n')
         for k in COMMANDS:
             print(k)
-
-    COMMANDS[command](file_name)
+    else:
+        COMMANDS[command](file_name)
 
 
 if __name__ == "__main__":
